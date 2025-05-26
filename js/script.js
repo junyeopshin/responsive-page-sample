246c668674af353aaf92a7210e2ca74cedc511b7
junyeopshin <macsin77@gmail.com>
2025-05-26 12:16:53 +0900

    isotope 라이브러리 기능 추가

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Isotope 인스턴스 객체 생성
+const sectionIso = new Isotope(".section", {
+  // options
+  itemSelector: "article",
+  masonry: {
+    // use element for option
+    columnWidth: "article",
+  },
+});
+
+const sortingBtns = document.querySelectorAll(".sorting > li");
+sortingBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    // 재정렬
+    const filter = e.currentTarget.querySelector("a").getAttribute("href");
+    sectionIso.arrange({
+      filter: filter,
+    });
+
+    // on클래스 모두 제거후 클릭한곳에 on클래스 추가
+    sortingBtns.forEach((btnEl) => {
+      btnEl.classList.remove("on");
+    });
+    e.currentTarget.classList.add("on");
+  });
+});
